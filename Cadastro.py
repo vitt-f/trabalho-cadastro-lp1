aa0eddf5d9b85caef93a4c4f5f0470d191edcb49
vitt-f <vitoriaferreiradealmeida1230@gmail.com>
2025-03-07 08:43:01 -0300

    commit cadastro completo

--- Cadastro.py
+++ Cadastro.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 import json
+import os
 
 lista_funcionarios = []
 
 def ler_valor_nao_vazio(nome_variavel):
-            valor_lido = input(f'Entre com o valor para {nome_variavel} do funcionario: ')
-            while valor_lido == '':
-                print(f'O valor para {nome_variavel} não pode ser vazio!')
-                valor_lido = input(f'Entre com o valor para {nome_variavel} do funcionario: ')
-            return valor_lido
+    valor_lido = input(f'Entre com o valor para {nome_variavel} do funcionario: ')
+    while valor_lido == '':
+        print(f'O valor para {nome_variavel} não pode ser vazio!')
+        valor_lido = input(f'Entre com o valor para {nome_variavel} do funcionario: ')
+    return valor_lido
 
 def verifica_data_valida(data_texto):
     try:
@@ -16,7 +17,7 @@ def verifica_data_valida(data_texto):
         return True
     except:
         return False
-    
+
 def ler_data_valida(nome_data):
     dataString = input(f"Entre com a data de {nome_data} do contrato: ")
     while not verifica_data_valida(dataString):
@@ -26,39 +27,50 @@ def ler_data_valida(nome_data):
     return data
 
 def listar_funcionarios(lista):
+    if not lista:
+        print("Nenhum funcionário cadastrado.")
     for i, funcionario in enumerate(lista):
         print(f"===== {i+1} =====")
         imprimir_funcionario(funcionario)
 
 def deletar_funcionario(lista):
-    listar_funcionarios
-    x = int(input('Digite o numero do funcionário que quer demitir: '))
-    lista_funcionarios.pop()
+    listar_funcionarios(lista)
+    try:
+        x = int(input('Digite o numero do funcionário que quer demitir: ')) - 1
+        if 0 <= x < len(lista):
+            lista.pop(x)
+            print("Funcionário removido com sucesso!")
+        else:
+            print("Número de funcionário inválido.")
+    except ValueError:
+        print("Digite um número válido.")
 
 def editar_funcionario(lista):
-    editX = int(input("Qual funcionário deseja editar? "))
-    editX -= 1
-    editY = input("O que deseja editar?\n 1- Nome\n 2- Data inicial\n 3- Data final\n 4- Cargo\n ")
-    if editY == "1":
-        novo_nome = ler_valor_nao_vazio('nome')
-        lista_funcionarios[editX]['nome'] = novo_nome 
-        print("Dados alterados com sucesso")
-        salvar_json(lista_funcionarios)
-    elif editY == "2":
-        nova_dataI = ler_valor_nao_vazio('entrada')
-        lista_funcionarios[editX]['entrada'] = nova_dataI
-        print("Dados alterados com sucesso")
-        salvar_json(lista_funcionarios)
-    elif editY == "3":
-        nova_dataF = ler_valor_nao_vazio('saída')
-        lista_funcionarios[editX]['saída'] = nova_dataF
-        print("Dados alterados com sucesso")
-        salvar_json(lista_funcionarios)
-    elif editY == "4":
-        novo_cargo = ler_valor_nao_vazio('cargo')
-        lista_funcionarios[editX]['cargo'] = novo_cargo
-        print("Dados alterados com sucesso")
-        salvar_json(lista_funcionarios)
+    listar_funcionarios(lista)
+    try:
+        editX = int(input("Qual funcionário deseja editar? ")) - 1
+        if 0 <= editX < len(lista):
+            editY = input("O que deseja editar?\n 1- Nome\n 2- Data inicial\n 3- Data final\n 4- Cargo\n ")
+            if editY == "1":
+                novo_nome = ler_valor_nao_vazio('nome')
+                lista[editX]['nome'] = novo_nome 
+            elif editY == "2":
+                nova_dataI = ler_data_valida('entrada')
+                lista[editX]['dataEntrada'] = nova_dataI.strftime('%d/%m/%Y')
+            elif editY == "3":
+                nova_dataF = ler_data_valida('saída')
+                lista[editX]['dataSaida'] = nova_dataF.strftime('%d/%m/%Y')
+            elif editY == "4":
+                novo_cargo = ler_valor_nao_vazio('cargo')
+                lista[editX]['cargo'] = novo_cargo
+            else:
+                print("Opção inválida.")
+            print("Dados alterados com sucesso")
+            salvar_json(lista)
+        else:
+            print("Número de funcionário inválido.")
+    except ValueError:
+        print("Digite um número válido.")
 
 def ler_funcionario():
     nome = ler_valor_nao_vazio('nome')
@@ -76,12 +88,12 @@ def ler_funcionario():
     return funcionario
 
 def ler_json():
-    with open('funcionarios.json', 'r') as meu_arquivo:
-        lista_funcionarios = json.load(meu_arquivo)
-    return lista_funcionarios
+    if os.path.exists('funcionarios.json'):
+        with open('funcionarios.json', 'r') as meu_arquivo:
+            return json.load(meu_arquivo)
+    return []  
 
 def salvar_json(lista_funcionarios):
-    
     with open("funcionarios.json", "w") as meu_arquivo:
         lista_json = json.dumps(lista_funcionarios, indent=4)
         meu_arquivo.write(lista_json)
@@ -92,25 +104,25 @@ def imprimir_funcionario(funcionario):
     print(f"Saída:\t\t{funcionario['dataSaida']}")
     print(f"Cargo:\t\t{funcionario['cargo']}")
     
-salvar_json(lista_funcionarios)
 lista_funcionarios = ler_json()
 
 while True:
-    salvar_json(lista_funcionarios)
     menu = input("1- Adicionar funcionário \n2- Ver funcionários \n3- Remover funcionário \n4- Editar funcionário \n5- Sair\n")
     if menu == '1':
         funcionario_1 = ler_funcionario()
         lista_funcionarios.append(funcionario_1)
+        salvar_json(lista_funcionarios)
         imprimir_funcionario(funcionario_1)
         
-    if menu == '2':
+    elif menu == '2':
         listar_funcionarios(lista_funcionarios)
 
-    if menu == "3":
-         deletar_funcionario(lista_funcionarios)
+    elif menu == "3":
+        deletar_funcionario(lista_funcionarios)
 
-    if menu == "4":
+    elif menu == "4":
         editar_funcionario(lista_funcionarios)
 
-    if menu == "5":
+    elif menu == "5":
+        salvar_json(lista_funcionarios)
         break
